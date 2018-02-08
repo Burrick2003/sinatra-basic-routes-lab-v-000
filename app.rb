74338a9742d2bfeed36f01e146d964e4e5dcc0e6
Blakeley Bond <burrick2003@yahoo.com>
2018-02-08 17:38:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,16 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/name' do
     "My name is Chicana"
-    resp.status = 200
   end
   get '/hometown' do
     "My hometown is Baltimore"
-    resp.status = 200
 
   end
   get '/favorite-song' do
     "My favorite song is Kars 4 Kids"
-    resp.status = 200
 
   end
 end
